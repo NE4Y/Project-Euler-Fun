1b9243a093c901a5f8d2853b191e917f7095b01d
Steffen Lindner <slindner1996@gmail.com>
2021-04-17 15:21:46 +0200

    Add solution

--- Problem-3/Rust/src/main.rs
+++ Problem-3/Rust/src/main.rs
@@ -1,3 +1,24 @@
+fn is_prime(n: u64) -> bool {
+    let mut prime: bool = true;
+
+    for i in 2..((n as f64).sqrt() as u64)+1 {
+        if n % i == 0 {
+            prime = false;
+            break;
+        }
+    }
+    prime
+}
+
 fn main() {
-    println!("Hello, world!");
+    let n: u64 = 600851475143;
+
+    for i in 2..n {
+        if n % i == 0 {
+            if is_prime(n / i) {
+                println!("Found: {}", n/i);
+                break;
+            }
+        }
+    }
 }
