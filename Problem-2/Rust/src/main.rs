00a71b3d44b52d37ff73c454b568232fe3bc3955
Steffen Lindner <slindner1996@gmail.com>
2021-04-17 14:10:00 +0200

    Add rust solution

--- Problem-2/Rust/src/main.rs
+++ Problem-2/Rust/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+
+    let limit: i32 = 4000000;
+
+    let mut fib_1: i32 = 1;
+    let mut fib_2: i32 = 1;
+
+    let mut sum: i32 = 0;
+
+    while fib_2 <= limit {
+        if fib_2 % 2 == 0 {
+            sum += fib_2;
+        }
+
+        let tmp: i32 = fib_2;
+
+        fib_2 = fib_1 + fib_2;
+        fib_1 = tmp;
+    }
+
+    println!("{}", sum);
 }
