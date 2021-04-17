b1d8073c6d04e3f5ae05beb368a198e05c6f7eb2
Steffen Lindner <slindner1996@gmail.com>
2021-04-17 16:25:24 +0200

    Add solution

--- Problem-4/Rust/src/main.rs
+++ Problem-4/Rust/src/main.rs
@@ -1,3 +1,39 @@
+fn number_to_vec(n: u32, v: &mut Vec<u32>) {
+    v.push(n % 10);
+
+    if n > 10 {
+        number_to_vec(n/10, v);
+    }
+}
+
+fn is_palindrom<T: Eq + Clone>(v1: &Vec<T>) -> bool {
+    let mut v2 = v1.clone();
+    v2.reverse();
+
+    return v1 == &v2;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut largest = 1;
+
+    for i in 100..1000 {
+        for j in 100..i {
+            let number = i * j;
+
+            let mut v1 = vec![];
+
+            number_to_vec(number, &mut v1);
+
+            if is_palindrom(&v1) {
+                if number > largest {
+                    largest = number;
+                }
+            }
+        }
+    }
+
+
+    println!("{}", largest);
+
+
 }
