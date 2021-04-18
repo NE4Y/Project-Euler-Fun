374280967f263c46e8743e13a4a546042b3444f3
Steffen Lindner <slindner1996@gmail.com>
2021-04-18 16:15:37 +0200

    Add solution for problem 5

--- Problem-5/Rust/src/main.rs
+++ Problem-5/Rust/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let mut i = 21;
+
+    loop {
+        let mut divisible = true;
+        for j in (2..21).rev() {
+            if i % j != 0 {
+                divisible = false;
+                break;
+            }
+        }
+
+        if divisible {
+            break;
+        }
+
+        i += 1;
+    }
+
+
+    println!("i: {}", i);
 }
