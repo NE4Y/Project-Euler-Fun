8b9ac953d0e55d6259db0159e5492a4e0014ec1e
Steffen Lindner <slindner1996@gmail.com>
2021-04-18 17:14:22 +0200

    Add solution to 7

--- Problem-7/Rust/src/main.rs
+++ Problem-7/Rust/src/main.rs
@@ -1,3 +1,24 @@
+fn is_prime(n: u32) -> bool {
+    for i in 2..(n as f64).sqrt() as u32 + 1 {
+        if n % i == 0 {
+            return false
+        }
+    }
+
+    true
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut counter = 1;
+    let mut i = 1;
+
+    while counter != 10001 {
+        i += 2;
+
+        if is_prime(i) {
+            counter += 1;
+        }
+    }
+
+    println!{"10 001st prime number is: {}", i};
 }
