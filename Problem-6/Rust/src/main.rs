d887c8d5f2f4430e032d893a3e7fd2bc90f25e71
Steffen Lindner <slindner1996@gmail.com>
2021-04-18 16:44:42 +0200

    Add solution to 6

--- Problem-6/Rust/src/main.rs
+++ Problem-6/Rust/src/main.rs
@@ -1,3 +1,19 @@
+fn gauss(n: u32) -> u32 {
+    (n * (n+1))/2
+}
+
 fn main() {
-    println!("Hello, world!");
+    let sum_of_squares = {
+        let mut sum: u32 = 0;
+
+        for i in 1u32..101 {
+            sum += i.pow(2);
+        }
+
+        sum
+    };
+
+    let square_of_sum: u32 = gauss(100).pow(2);
+
+    println!("Difference is: {}", square_of_sum - sum_of_squares);
 }
